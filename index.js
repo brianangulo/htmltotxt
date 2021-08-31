9f7fc166faa36612954a1f939e3f382c0edbba15
Brian Angulo <brian@popped.com>
2021-08-30 22:25:21 -0500

    switched to commojs

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import { writeFile, readFileSync } from "fs";
+const writeFile = require("fs").writeFile;
+const readFileSync = require('fs').readFileSync;
 /**
  * Basic parser extracts text from html files
  * @param {string} path The path to the html file to read from
  * @returns {string} With all of the HTML nodes removed
  */
-export function parser(path) {
+function parser(path) {
   const fullString = readFileSync(path).toString();
   const regex = /<.+?>/;
   return fullString.split(regex).join("");
@@ -13,7 +14,7 @@ export function parser(path) {
  * Generator of .txt files in same directory
  * @param {string} str A plain string to generate txt from
  */
-export function generator(str) {
+function generator(str) {
   const randomNum = Math.floor(Math.random() * 50);
   writeFile(`fromHtml${randomNum}.txt`, str, "utf8", (err) => console.log(err));
   return "file generated";
@@ -21,4 +22,9 @@ export function generator(str) {
 
 exports.printMsg = function () {
   console.log("This is a message from the demo package");
+};
+
+module.exports = {
+  parser,
+  generator,
 };
